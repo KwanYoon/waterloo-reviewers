f881ad75c5500248369b59d6e912ed525c25a641
Kwan Yoon <kwanyoon2015@gmail.com>
2022-06-05 20:38:39 -0400

    initial commit

---
+++ src/containers/HomePage/HomePage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./HomePage.styles.scss";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>homepage</h1>
+    </div>
+  );
+}
+
+export default HomePage;

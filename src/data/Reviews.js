8eb68f572620305eea9dc7150884664c46c792b4
Kwan Yoon <kwanyoon2015@gmail.com>
2022-06-09 20:03:36 -0400

    added data

---
+++ src/data/Reviews.js
@@ -0,0 +1,14 @@
+export const Reviews = [
+  {
+    "name": "Wendy's",
+    "rating": 10,
+  },
+  {
+    "name": "Panda Express",
+    "rating": 8,
+  },
+  {
+    "name": "Sowon's",
+    "rating": 9,
+  }
+];

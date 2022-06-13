e03e210b2ce59da9b6c174cf002dccc7d7a17f99
Kwan Yoon <kwanyoon2015@gmail.com>
2022-06-13 19:47:41 -0400

    Added blogpage and changed Review format

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
+import BlogPage from './containers/BlogPage/BlogPage.component';
 import HomePage from './containers/HomePage/HomePage.component';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/blogs/:blogName" element={<BlogPage />} />
       </Routes>
     </div>
   );
--- src/data/Reviews.js
+++ src/data/Reviews.js
@@ -1,14 +1,65 @@
 export const Reviews = [
   {
+    "key": "wendys",
     "name": "Wendy's",
-    "rating": 10,
+    "ratings": [
+      {
+        "name": "Kwan",
+        "score": 9,
+        "description": "Probably the best fast food restaurant",
+      },
+      {
+        "name": "Diyar",
+        "score": 5,
+        "description": "I think Burger King is better",
+      },
+      {
+        "name": "Jaden",
+        "score": 10,
+        "description": "mmm",
+      },
+    ],
   },
   {
+    "key": "panda-express",
     "name": "Panda Express",
-    "rating": 8,
+    "ratings": [
+      {
+        "name": "Kwan",
+        "score": 4,
+        "description": "Chinese food is too oily",
+      },
+      {
+        "name": "Diyar",
+        "score": 7,
+        "description": "Pretty good",
+      },
+      {
+        "name": "Jaden",
+        "score": 10,
+        "description": "Love China",
+      },
+    ],
   },
   {
+    "key": "sowons",
     "name": "Sowon's",
-    "rating": 9,
+    "ratings": [
+      {
+        "name": "Kwan",
+        "score": 9,
+        "description": "Probably the best chicken in Canada",
+      },
+      {
+        "name": "Diyar",
+        "score": 10,
+        "description": "Love Sowons",
+      },
+      {
+        "name": "Jaden",
+        "score": 8,
+        "description": "Love chicken",
+      },
+    ],
   }
 ];

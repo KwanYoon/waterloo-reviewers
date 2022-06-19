71b00de0004f51bfb5897857efc329dd98ca0adf
Diyar Yasin <47333278+Diyar-Yasin@users.noreply.github.com>
2022-06-19 17:19:40 -0400

    Linked search results from search bar to blog post pages

--- src/components/Searchbar/Searchbar.component.jsx
+++ src/components/Searchbar/Searchbar.component.jsx
@@ -5,11 +5,12 @@ import "./Searchbar.styles.scss";
 import SearchIcon from "./Images/search-icon.png";
 import ClearIcon from "./Images/clear-icon.png";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Searchbar = () => {
+    const navigate = useNavigate();
     const [userInput, setUserInput] = useState("");
     const [searchResults, setSearchResults] = useState(Reviews);
-    const [showSearchResults, setShowSearchResults] = useState(false);
 
     function getMatchedResults(inputText)
     {
@@ -19,26 +20,45 @@ const Searchbar = () => {
             return reviewName.includes(inputText.toLowerCase())});
     }
 
-    function  handleChange(text) {
-        setUserInput(text);
-        setSearchResults(getMatchedResults(text));
+    const onSearch = (searchItem) => {
+        setUserInput(searchItem.name);
+
+        navigate("/blogs/" + searchItem.key);
     }
 
-    function handleBlur() {
-        setShowSearchResults(false); 
+    function handleChange(text) {
+        setUserInput(text);
+        setSearchResults(getMatchedResults(text));
     }
     
     return (
         <div className="main">
-            <img className="search-icon" src={SearchIcon} alt=""/>
-            <input className="search-text-box" value={userInput} placeholder="Search restaurants, cafes, etc" onFocus={e => setShowSearchResults(true)} onBlur={e => setTimeout(handleBlur, 1000)} onChange={e => (handleChange(e.target.value))}/>
-            {
-                showSearchResults ? (searchResults.length > 0 || userInput.length === 0 ? searchResults.map((post) => (
-                                        <div className="blog-list-item" key={post.key}>
-                                            <Link to={"/blogs/" + post.key }>{post.name}</Link>
-                                        </div>)) :
-                                        <p>Sorry, it looks like we haven't reviewed what you're looking for!</p>) : null
-            }
+            <div className="search-inner">
+                <input className="search-text-box" value={userInput} placeholder="Search restaurants, cafes, etc" onChange={e => (handleChange(e.target.value))}/>
+            </div>
+            <div className="dropdown">
+                {Reviews
+                .filter((item) => {
+                const searchTerm = userInput.toLowerCase();
+                const fullName = item.name.toLowerCase();
+
+                return (
+                    searchTerm &&
+                    fullName.includes(searchTerm) &&
+                    fullName !== searchTerm
+                );
+                })
+                .slice(0, 10)
+                .map((item) => (
+                <div
+                    onClick={() => onSearch(item)}
+                    className="dropdown-row"
+                    key={item.key}
+                >
+                    {item.name}
+                </div>
+                ))}
+            </div>
         </div>
     )
 }
@@ -47,5 +67,14 @@ export default Searchbar;
 /*
 {
                 userInput.length > 0 ? <img className="clear-icon" src={ClearIcon} alt="" onClick={() => handleChange("")}/> : null
+            }
+
+            <img className="search-icon" src={SearchIcon} alt=""/>
+                        {
+                showSearchResults ? (searchResults.length > 0 || userInput.length === 0 ? searchResults.map((post) => (
+                                        <div className="blog-list-item" key={post.key}>
+                                            <Link to={"/blogs/" + post.key }>{post.name}</Link>
+                                        </div>)) :
+                                        <p>Sorry, it looks like we haven't reviewed what you're looking for!</p>) : null
             }
 */

d8c551271ee437488d57dc8c30f6a0b7eaffa5df
Diyar Yasin <47333278+Diyar-Yasin@users.noreply.github.com>
2022-06-24 15:58:10 -0400

    added navbar

--- src/containers/HomePage/HomePage.component.jsx
+++ src/containers/HomePage/HomePage.component.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import Searchbar from "./../../components/Searchbar/Searchbar.component.jsx"
 import "./HomePage.styles.scss";
+import Navbar from './../../components/Navbar/Navbar.component';
 
 const HomePage = () => {
   return (
     <div className="homepage">
-
+      <Navbar />
       <h1 className="title">Waterloo Reviewers</h1>
-      <div className="searchbar">
-        <Searchbar />
-      </div>
+      <Searchbar className="searchbar" />
     </div>
   );
 }
